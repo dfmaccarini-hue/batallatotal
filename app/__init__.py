2b099ce2b1b810236c3061a823e3e4cf5c59161d
dfmaccarini-hue <dfmaccarini@gmail.com>
2026-09-05 16:15:47 -0300

    subo .py actualizado con database_URL

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 # Inicializar la base de datos
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
     # Configuración básica
     app.config['SECRET_KEY'] = 'clave-secreta-mvp'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///outbid.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inicializar extensiones
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Importar modelos para que se registren
     from app import models
@@ -23,3 +27,4 @@ def create_app():
     app.register_blueprint(leaderboard_bp)
 
     return app
+
